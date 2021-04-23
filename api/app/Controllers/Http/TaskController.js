6d1757892ac20c55bd4b88b9efe8346884b9d87d
Pablo Sanches <pablo_sanches@outlook.com>
2021-04-23 14:34:03 -0300

    cria o modelo e controller de task

---
+++ api/app/Controllers/Http/TaskController.js
@@ -0,0 +1,89 @@
+'use strict'
+
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with tasks
+ */
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Task = use('App/Models/Task')
+
+class TaskController {
+  /**
+   * Show a list of all tasks.
+   * GET tasks
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async index ({ request, response}) {
+    const tasks = await Task.all()
+
+    return tasks
+  }
+
+  /**
+   * Create/save a new task.
+   * POST tasks
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+    const info = request.only(['titulo', 'descricao'])
+    const task = await Task.create(info)
+
+    return task
+  }
+
+  /**
+   * Display a single task.
+   * GET tasks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async show ({ params, request, response}) {
+    return Task.findOrFail(params.id)
+  }
+
+  /**
+   * Update task details.
+   * PUT or PATCH tasks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+    const task = Task.findOrFail(params.id)
+    const info = request.only(['titulo', 'descricao'])
+    task.merge(info)
+
+    await task.save()
+
+    return task
+  }
+
+  /**
+   * Delete a task with id.
+   * DELETE tasks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+    const task = Task.findOrFail(params.id)
+
+    await task.delete()
+  }
+}
+
+module.exports = TaskController
